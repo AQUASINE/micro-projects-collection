9bf2003243aee5b260641a65ff7a234c839702f6
Sean Chalmers <schalmers3@gatech.edu>
2023-01-01 21:01:47 -0600

    content: add items to list

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,10 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production' ? '/micro-projects-collection/' : '/',
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      title: 'Berggren Stream Stuff',
+    }
+  }
 })
